b170fd4ae7eb2d309f2a229214f0df94973fb8c6
Jhayr <jhayrcerna10@gmail.com>
2024-04-16 22:50:50 -0500

    hola :D

--- src/vista/Admin.java
+++ src/vista/Admin.java
@@ -16,7 +16,7 @@ public class Admin extends javax.swing.JFrame {
     public Admin() {
         initComponents();
     }
-
+//holaaaaaaaaaaaaa
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
