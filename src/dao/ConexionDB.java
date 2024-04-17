d6b475f43aa80d1c5edab81018efbd5d9d477dbf
Jhayr <jhayrcerna10@gmail.com>
2024-04-17 13:07:23 -0500

    + ParametrosBD en el package DAO

--- src/dao/ConexionDB.java
+++ src/dao/ConexionDB.java
@@ -1,6 +1,19 @@
 
 package dao;
-
-public class ConexionDB {
-    
+/*import java.sql.*;
+import javax.swing.JOptionPane; */
+public class ConexionDB implements ParametrosBD{
+   /*Connection conexion;   
+    Statement st;
+    ResultSet rs;
+    PreparedStatement ps;
+    public ConexionDB(){
+        try {
+            Class.forName(DRIVE);
+            conexion = DriverManager.getConnection(RUTA,USUARIO,CLAVE);   
+            st=conexion.createStatement();            
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "No se pudo conectar a la base de datos");
+        }
+    }*/
 }
---
+++ src/dao/ParametrosBD.java
@@ -0,0 +1,8 @@
+package dao;
+
+public interface ParametrosBD {
+        /*String DRIVE="com.mysql.cj.jdbc.Driver";
+        String RUTA="jdbc:mysql://localhost:3306/FarmaciaTodo";
+        String USUARIO="root";
+        String CLAVE="";
+*/}
